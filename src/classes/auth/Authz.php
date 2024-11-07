77a53bf00dd5765f4394fb4ce0b116a0256ce08e
EliasKho <elias.khodjaoui@free.fr>
2024-11-07 14:37:33 +0100

    classe Authz

--- src/classes/auth/Authz.php
+++ src/classes/auth/Authz.php
@@ -2,7 +2,25 @@
 
 namespace iutnc\nrv\auth;
 
+use iutnc\nrv\user\User;
+
 class Authz
 {
+    private User $user;
 
+    public function __construct(User $user) {
+        $this->user = $user;
+    }
+
+    /**
+     * Vérifie si l'utilisateur est un administrateur
+     * @return bool
+     */
+    public function isAdmin(): bool {
+        return $this->user->role === 'ADMIN';
+    }
+
+    public function isStaff(): bool {
+        return $this->user->role === 'STAFF';
+    }
 }
--- src/classes/user/User.php
+++ src/classes/user/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace iutnc\deefy\compte;
+namespace iutnc\nrv\user;
 
 class User
 {
