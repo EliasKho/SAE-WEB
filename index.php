a3afe7098e24ec22a578b789c534d16ce5a52759
Léo Fontaine <leo.fontaine4@orange.fr>
2024-11-07 14:27:27 +0100

    finalisation index

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 declare (strict_types = 1);
 require_once 'loader/vendor/autoload.php';
 
+iutnc\nrv\repository\NRVRepository::setConfig( 'config.db.ini' );
+
 $dispatcher = new iutnc\nrv\dispatcher\Dispatcher();
 $dispatcher->run();
