2d2ad16c57a2c04115ce279559cf02d4d49d9fe2
Léo Fontaine <leo.fontaine4@orange.fr>
2024-11-07 13:43:32 +0100

    autoloader et repertoires

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iutnc/nrv',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8f6db4f6bfac9a56bdcdc8c135dbde2ecc8594f7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iutnc/nrv' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8f6db4f6bfac9a56bdcdc8c135dbde2ecc8594f7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
