531db0bbee026243b93be9012ca6767a88d9bd29
Léo Fontaine <leo.fontaine4@orange.fr>
2024-11-07 14:23:20 +0100

    ajout repository et gitignore

--- src/classes/repository/NRVRepository.php
+++ src/classes/repository/NRVRepository.php
@@ -2,7 +2,37 @@
 
 namespace iutnc\nrv\repository;
 
-class NRVRepository
-{
+class NRVRepository{
+    private \PDO $pdo;
+    private static ?NRVRepository $instance = null;
+    private static array $config = [];
+
+    private function __construct(array $conf)
+    {
+        $this->pdo = new \PDO($conf['dsn'], $conf['user'], $conf['pass'],
+            [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'"]);
+    }
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new NRVRepository(self::$config);
+        }
+        return self::$instance;
+    }
+
+    public static function setConfig(string $file)
+    {
+        $conf = parse_ini_file($file);
+        if ($conf === false) {
+            throw new \Exception("Error reading configuration file");
+        }
+        $driver = $conf['driver'];
+        $host = $conf['host'];
+        $database = $conf['database'];
+        $dsn = "$driver:host=$host;dbname=$database";
+        self::$config = ['dsn' => $dsn, 'user' => $conf['user'], 'pass' => $conf['pass']];
+    }
 
 }
