d7a5715f8bf2bd62e10070bb428ad5e6d731c52a
alexxela57 <alexandre.hary3@etu.univ-lorraine.fr>
2024-11-12 15:31:35 +0100

    ajout bouton annuler et rétablir spectacle

---
+++ src/classes/action/AnnulerRetablirSpectacle.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace iutnc\nrv\action;
+
+use iutnc\nrv\repository\NRVRepository;
+use iutnc\nrv\festival\Spectacle;
+use iutnc\nrv\action\Spectacles;
+use iutnc\nrv\action as ACT;
+
+
+class AnnulerRetablirSpectacle extends Action
+{
+    protected function executeGet(): string
+    {
+        $idSpectacle = $_GET['idSpectacle'] ?? null;
+        if (!$idSpectacle) {
+            return "ID du spectacle manquant.";
+        }
+
+        // Récupérer le spectacle depuis le dépôt
+        $repository = NRVRepository::getInstance();
+        $spectacle = $repository->getSpectacleById($idSpectacle);
+
+        if ($spectacle) {
+            // Basculer l'état d'annulation
+            if ($spectacle->estAnnule) {
+                $spectacle->retablir();
+                $message = "Le spectacle a été rétabli.";
+            } else {
+                $spectacle->annuler();
+                $message = "Le spectacle a été annulé.";
+            }
+
+            // Enregistrer la mise à jour dans la base de données
+            $repository->updateSpectacle($spectacle);
+
+            echo "<script>window.alert ('$message');</script>";
+            $act = new ACT\Spectacles();
+            return $act->executeGet();
+        }
+
+        return "Spectacle introuvable.";
+    }
+
+    protected function executePost(): string
+    {
+        return $this->executeGet();
+    }
+}
